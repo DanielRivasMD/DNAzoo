6cbee2a66123a03670222db94a565a87f4140939
DanielRivasMD <danielrivasmd@gmail.com>
2022-04-17 15:39:41 +0200

    add docker controller test

--- src/dockerController.sh
+++ src/dockerController.sh
@@ -10,4 +10,7 @@ docker run --rm -v ~/Factorem/DNAzoo/uploads:/data -e JAVA_TOOLS_OPTIONS-"-Xms2G
 # retrieve fields
 docker run --rm -v ~/Factorem/DNAzoo/uploads:/data -e JAVA_TOOLS_OPTIONS-"-Xms2G" enasequence/webin-cli -context=genome -fields
 
+# test
+docker run --rm -v ~/Factorem/DNAzoo/uploads:/data -e JAVA_TOOLS_OPTIONS-"-Xms2G" enasequence/webin-cli -context=genome -userName=Webin-62042 -passwordFile=data/.password.txt -outputDir=data/report -test -validate -manifest=data/manifest/MacropusEugenii
+
 ####################################################################################################
